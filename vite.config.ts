4082bbbf8e6e0585e25909ecf0a6e355e3b2764e
madebyvanta <madebyvanta@gmail.com>
2025-11-20 15:35:58 -0600

    prueba nueva 3

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      // ESTA LÍNEA CORRIGE LA PANTALLA NEGRA:
-      // Le dice a Vite que busque los archivos (js, css) usando rutas relativas
-      // en lugar de absolutas (que fallan al estar en una subcarpeta).
-      base: './', 
-      
       server: {
         port: 3000,
         host: '0.0.0.0',
